6a5c0435bdec506e081e5879ed0afeca1d61c931
naser-kazemi <naserkazemi2002@gmail.com>
2021-05-03 18:08:28 +0430

    update

--- src/main/java/model/Card.java
+++ src/main/java/model/Card.java
@@ -1,4 +1,22 @@
 package model;
 
 public class Card {
+
+    protected String cardName;
+    protected String description;
+    protected String status;
+    protected String statusOnField;
+    protected String cardType;
+
+
+    public Card createCard(String cardType, String cardName) {
+
+    }
+
+    public String getCardType() {
+        return this.cardType;
+    }
+
+
+
 }
