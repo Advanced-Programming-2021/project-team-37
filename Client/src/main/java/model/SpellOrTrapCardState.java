fd5c6531df54cd8d298de7b11d3cc5020796332a
Moein72002 <moeinmadadi2002@gmail.com>
2021-07-14 13:20:31 +0430

    phase3_2

--- Client/src/main/java/model/SpellOrTrapCardState.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public enum SpellOrTrapCardState {
-    ACTIVATED,
-    INACTIVATED,
-    SET
-}
--- Server/src/main/java/model/commandClasses/CommandClass.java
+++
@@ -1,9 +0,0 @@
-package model.commandClasses;
-
-import model.CommandType;
-
-public class CommandClass {
-    private String message; // this is the message that we want to show to user
-    private CommandType commandType; // this is for knowing current page
-
-}
